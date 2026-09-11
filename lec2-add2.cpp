41178a36b8f82bd18ac280c4e11605f6f6a3032d
ttalgi26 <138685056+ttalgi26@users.noreply.github.com>
2026-09-11 12:03:30 +0900

    Improve input prompt formatting in lec2-add2.cpp
    Fix formatting of input prompts for clarity.

--- lec2-add2.cpp
+++ lec2-add2.cpp
@@ -1,3 +1,10 @@
+/*
+ 
+ scanf(형식 지정자, &변수);
+ &연산자 : 변수의 주소를 계산하는 연산자
+
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
@@ -9,9 +16,9 @@ int main(void)
     int x;
     int y;
     int sum;
-    printf("첫 번째 숫자를입력하시오:");
+    printf("첫 번째 숫자를입력하시오: ");
     scanf("%d", &x);
-    printf("두 번째 숫자를입력하시오:");
+    printf("두 번째 숫자를입력하시오: ");
     scanf("%d", &y);
     sum = x + y;
     printf("두 숫자의 합은 = %d\n", sum);
