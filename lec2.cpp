af5718090ab15219716adda508581499e6c3667e
ttalgi26 <138685056+ttalgi26@users.noreply.github.com>
2026-09-11 10:13:09 +0900

    Create lec2.cpp

---
+++ lec2.cpp
@@ -0,0 +1,13 @@
+/*
+프로그래밍 기초 - 2주차 수업 
+(대충 이렇게 주석달면 크게 할 수 있다는 내용)
+*/
+//이것도 주석임
+#include <stdio.h>	//standard input/output
+
+int main(void) 
+{
+	printf("Hello World!\n");
+	printf("from YeJun\n");
+	return 0;
+}
