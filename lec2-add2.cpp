aaeb9112397f76639d5e333aab80a93258ca278e
ttalgi26 <138685056+ttalgi26@users.noreply.github.com>
2026-09-11 11:42:53 +0900

    Create lec2-add2.cpp

---
+++ lec2-add2.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+
+int main(void)
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    int x;
+    int y;
+    int sum;
+    printf("첫 번째 숫자를입력하시오:");
+    scanf("%d", &x);
+    printf("두 번째 숫자를입력하시오:");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("두 숫자의 합은 = %d\n", sum);
+
+    return 0;
+}
